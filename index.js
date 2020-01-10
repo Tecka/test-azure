10dda72d8dd969e40e360e9c5256c68d22a76127
20412791 <bast.rx@gmail.com>
2020-01-10 12:30:47 +0100

    - iframe

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, { "Content-Type": "text/html" });
-    response.end("<html><body><h1>Power BI test!</h1><br><iframe width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=036e2b42-241d-4888-8489-071c763663fa&autoAuth=true&ctid=8b9cef57-e6fc-499a-9ff8-45c1cf0dd671&config=eyJjbHVzdGVyVXJsIjoiaHR0cHM6Ly93YWJpLXdlc3QtZXVyb3BlLWItcHJpbWFyeS1yZWRpcmVjdC5hbmFseXNpcy53aW5kb3dzLm5ldC8ifQ%3D%3D" frameborder="0" allowFullScreen="true"></iframe></body></html>");
+    response.end("<html><body><h1>Power BI test!</h1><br></body></html>");
 
 });
 
